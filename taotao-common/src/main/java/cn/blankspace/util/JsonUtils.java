9cbfb02e1aa1ab536d9bc542bf6b12d5665777fc
BlankSpace <1042079408@qq.com>
2018-10-01 19:14:41 +0800

    10.1内容管理

--- taotao-common/src/main/java/cn/blankspace/util/JsonUtils.java
+++ taotao-common/src/main/java/cn/blankspace/util/JsonUtils.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.taotao.result.TaotaoResult;
+
 
 /**
  * 淘淘商城自定义响应结构
